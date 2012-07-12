a6de0bd7f3ddada99504feab0bf74b1a1f393f7c
Jamie Ly <jamie.ly@gmail.com>
2012-07-12 08:16:43 -0400

    Added variables to models, making spec pass.

--- lib/simulator/model.rb
+++ lib/simulator/model.rb
@@ -2,11 +2,17 @@ module Simulator
   #
   class Model
     attr_reader :equations
+    attr_reader :variables
+
     def initialize
       @equations = []
+      @variables = []
     end
     def add_equation(equation)
       @equations << equation
+    end
+    def add_variable(variable)
+      @variables << variable
     end
   end
 end

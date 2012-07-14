bd0552092b49ba6b5144f41fbff2a29bbffe51ef
Jamie Ly <jamie.ly@gmail.com>
2012-07-13 22:30:43 -0400

    Made some methods in sandbox private

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ group :development do
   gem 'guard-cucumber'
   gem 'guard-rspec'
   gem 'ruby_gntp'
+  gem 'pry'
 end
 group :test do
   gem "rspec"
--- lib/simulator/sandbox.rb
+++ lib/simulator/sandbox.rb
@@ -4,17 +4,15 @@ module Simulator
       @context = context
     end
 
+    private
+
     def respond_to?(sym)
       puts "Respond to: #{sym}"
       not @context.get(sym.to_sym).nil?
     end
 
     def method_missing(method_name, *args, &block)
-      #should_set = method_name =~ /=/
-      #puts "\nMethod: #{method_name}"
-      #method_name = method_name.to_s.gsub(/=$/, '').to_sym if should_set
       var = @context.get method_name.to_sym
-      #var.value = args.first if should_set
       return var.value unless var.nil?
       super(method_name, *args, &block)
     end
